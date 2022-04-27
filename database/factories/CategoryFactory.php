4e9feafa97635b8b3aef356e7c35c29622b52cd4
Javier Hernandez <javierxhernandezx@gmail.com>
2022-04-27 17:45:49 +0000

    Add Factorys and seeders for test data

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $name = $this->faker->unique()->word(20);
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Storage::deleteDirectory('public/posts');
+
+        $this->call(UserSeeder::class);
+
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+
+        Storage::makeDirectory('public/posts');
+
+        $this->call(PostSeeder::class);
     }
 }
